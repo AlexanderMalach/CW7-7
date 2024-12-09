1a04625cd5708d081cbb8949d1e03a05311957de
79096 <a.malakhinsky@gmail.com>
2024-12-09 20:52:10 +0300

    -

---
+++ habits/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """
+    Админка модели Habit
+    """
+    list_display = (
+        "id",
+        "habit",
+        "sign_of_a_pleasant_habit",
+        "related_habit",
+        "reward",
+    )
+    list_filter = ("sign_of_a_pleasant_habit",)
+    search_fields = ("habit",)
+    ordering = ("id",)
